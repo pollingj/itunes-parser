5864a87984ff6ea328f5bb418cf23fe35d473581
John Polling <john@theled.co.uk>
2016-06-28 09:35:10 +0100

    Renaming

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ITunesParser.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ITunesParser,
+    [app: :itunesparser,
      version: "0.1.0",
      elixir: "~> 1.0",
      description: "Elixir RSS/Atom parser built on erlang's xmerl xml parser",
